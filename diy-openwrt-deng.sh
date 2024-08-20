2d94ba3e6774675e96926a66b114657a327a7a59
y12800 <115250636+y12800@users.noreply.github.com>
2024-08-20 14:53:57 +0800

    Update diy-openwrt-deng.sh

--- diy-openwrt-deng.sh
+++ diy-openwrt-deng.sh
@@ -27,6 +27,8 @@ git clone --depth 1 https://github.com/y12800/luci-app-filebrowser package/fileb
 # git clone --depth 1 https://github.com/zhengmz/luci-app-zerotier.git package/luci-app-zerotier
 # git clone --depth 1 https://github.com/0x676e67/luci-app-watchcat-plus package/luci-app-watchcat-plus
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 https://github.com/ZeaKyX/luci-app-speedtest-web.git package/luci-app-speedtest-web
+git clone --depth=1 https://github.com/ZeaKyX/speedtest-web.git package/speedtest-web
 
 # git clone --depth 1 https://github.com/kenzok8/small-package deng-tmp1 && mv deng-tmp1/wrtbwmon package/deng/wrtbwmon && mv deng-tmp1/dnsforwarder package/deng/dnsforwarder
 # git clone --depth 1 https://github.com/messense/aliyundrive-webdav deng-tmp2 && mv deng-tmp2/openwrt package/deng/aliyundrive-webdav
