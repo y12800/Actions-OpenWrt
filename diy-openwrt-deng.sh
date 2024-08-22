2b4d1b08864b7a202f1ff8471af3fe3bfc07e4f4
y12800 <115250636+y12800@users.noreply.github.com>
2024-08-22 23:14:37 +0800

    Update diy-openwrt-deng.sh

--- diy-openwrt-deng.sh
+++ diy-openwrt-deng.sh
@@ -23,9 +23,9 @@ git clone --depth 1 https://github.com/lisaac/luci-app-dockerman package/deng/lu
 # git clone --depth 1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/deng/luci-theme-opentomcat
 git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky.git package/deng/luci-app-lucky
 # git clone --depth 1 https://github.com/y12800/luci-app-filebrowser package/filebrowser
-# git clone --depth 1 https://github.com/xiaozhuai/luci-app-filebrowser
-git clone --depth 1 https://github.com/sbwml/openwrt-filebrowser package/luci-app-filebrowser
-git clone --depth 1 https://github.com/sbwml/openwrt-filebrowser package/filebrowser
+git clone --depth 1 https://github.com/xiaozhuai/luci-app-filebrowser
+# git clone --depth 1 https://github.com/sbwml/openwrt-filebrowser package/luci-app-filebrowser
+# git clone --depth 1 https://github.com/sbwml/openwrt-filebrowser package/filebrowser
 
 git clone --depth 1 https://github.com/immortalwrt-collections/luci-app-diskman.git package/deng/luci-app-diskman
 # git clone --depth 1 https://github.com/zhengmz/luci-app-zerotier.git package/luci-app-zerotier
