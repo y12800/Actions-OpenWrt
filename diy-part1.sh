c9fa82bc7a15b891fd784e24308a0d831849aaf7
y12800 <115250636+y12800@users.noreply.github.com>
2024-09-01 22:44:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,17 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# {{ Add luci-app-diskman
+(cd openwrt && {
+    mkdir -p package/luci-app-diskman
+    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+    mkdir -p package/parted
+    wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+})
+# cat >> configs/rockchip/01-nanopi <<EOL
+# CONFIG_PACKAGE_luci-app-diskman=y
+# CONFIG_PACKAGE_luci-app-diskman_INCLUDE_btrfs_progs=y
+# CONFIG_PACKAGE_luci-app-diskman_INCLUDE_lsblk=y
+# CONFIG_PACKAGE_smartmontools=y
+# EOL
+# }}
