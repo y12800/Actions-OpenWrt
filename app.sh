4ac7b3772e9a0bfb2bebb66fd7cbfac7c7633476
y12800 <115250636+y12800@users.noreply.github.com>
2024-09-01 23:32:05 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,7 +4,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # {{ Add luci-app-diskman
-(cd openwrt && {
+(mkdir app
+cd app && {
     mkdir -p package/luci-app-diskman
     wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
     mkdir -p package/parted
