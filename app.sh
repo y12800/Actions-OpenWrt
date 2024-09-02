ce554c9d976a3d20e4d92e12a14ca3b16c852829
y12800 <115250636+y12800@users.noreply.github.com>
2024-09-02 08:04:36 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -5,10 +5,19 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # {{ Add luci-app-diskman
 
-mkdir -p package/luci-app-diskman
+mkdir -p package/luci-app-diskman && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
-mkdir -p package/parted
+mkdir -p package/parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+
+#compile package only
+make package/luci-app-diskman/compile V=99
+
+#compile
+make menuconfig
+#choose LuCI ---> 3. Applications  ---> <*> luci-app-diskman..... Disk Manager interface for LuCI ----> save
+make V=99
+
 
 
 # (cd app && {
