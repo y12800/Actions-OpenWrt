da27cfea776840e67f80b12479ca74a5775875ce
y12800 <115250636+y12800@users.noreply.github.com>
2024-11-23 11:32:36 +0800

    Update OpenWrt-22.03.sh

--- OpenWrt-22.03.sh
+++ OpenWrt-22.03.sh
@@ -9,7 +9,7 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/d
 git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha package/deng/luci-theme-alpha
 git clone --depth 1 https://github.com/derisamedia/luci-app-alpha-config package/deng/luci-app-alpha-config
 # git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/deng/luci-app-unblockneteasemusic
-# git clone --depth 1 https://github.com/sirpdboy/netspeedtest.git package/deng/netspeedtest
+git clone --depth 1 https://github.com/sirpdboy/netspeedtest package/deng/netspeedtest
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/deng/luci-app-passwall
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 package/deng/luci-app-passwall2
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/deng/passwall-packages
