6608e3137d9260079cd7c5fedd32b9e2c5875a1e
y12800 <115250636+y12800@users.noreply.github.com>
2025-06-20 13:03:56 +0800

    Update OpenWrt-24.10.sh

--- OpenWrt-24.10.sh
+++ OpenWrt-24.10.sh
@@ -61,7 +61,7 @@ git clone --depth 1 https://github.com/pymumu/luci-app-smartdns.git package/luci
 git clone --depth 1 https://github.com/sirpdboy/luci-app-cupsd.git package/deng/luci-app-cupsd
 # git clone --depth 1 https://github.com/y12800/zerotier package/deng/zerotier
 git clone --depth 1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
+sed -i 's/EXTRA_CFLAGS:=.*/& -Wno-error=misleading-indentation/' package/OpenAppFilter/oaf/Makefile
 # git clone --depth 1 https://github.com/zhengmz/luci-app-zerotier.git package/luci-app-zerotier
 # git clone --depth 1 https://github.com/0x676e67/luci-app-watchcat-plus package/luci-app-watchcat-plus
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
