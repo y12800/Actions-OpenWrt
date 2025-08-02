113d6f2010fca7d8996cdf041043eac9d26f20e1
y12800 <115250636+y12800@users.noreply.github.com>
2025-08-02 20:08:53 +0800

    Update OpenWrt-24.10.sh

--- OpenWrt-24.10.sh
+++ OpenWrt-24.10.sh
@@ -14,7 +14,7 @@ rm -rf feeds/packages/net/zerotier
 # rm -rf feeds/packages/net/socat
 # rm -rf feeds/luci/applications/luci-i18n-filebrowser-zh-cn
 sed -i 's/--set=llvm.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-# sed -i 's/--set=llvm.download-ci-llvm=.*/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
+
 
 # git clone --depth 1 https://github.com/vison-v/luci-app-nginx-proxy package/luci-app-nginx-proxy
 # git clone --depth 1 https://github.com/Vera2016/luci-app-nginx-manager package/luci-app-nginx-manager
